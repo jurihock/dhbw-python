026c8a0df2043df6738b530b9467b5b9e1210c35
Jürgen Hock <juergen.hock@jurihock.de>
2022-01-29 19:12:29 +0100

    Add comments

--- dhbw/dasp/signal.py
+++ dhbw/dasp/signal.py
@@ -2,41 +2,34 @@ import numpy
 import scipy.stats
 
 
-def noise(s, t):
-
-    a = -1 / s
-    b = +1 / s
-    n = len(t)
-
-    crv = scipy.stats.truncnorm(a, b, loc=0, scale=s)
-    return crv.rvs(n)
-
-
 def harmonic(f, t):
+    """
+    Returns a sine wave of the specified frequency.
+
+    Parameters
+    ----------
+    f : float
+        Signal frequency in hertz.
+    t : array
+        Timeline array.
+    """
 
     return numpy.sin(2 * numpy.pi * f * t)
 
 
-def square(f, t):
+def harmonics(f, n, t):
+    """
+    Returns a harmonic wave including the specified amount of overtones.
 
-    return numpy.sign(harmonic(f, t))
-
-
-def sawtooth(f, t):
-
-    return 2 * (f * t - numpy.floor(f * t + 0.5))
-
-
-def triangle(f, t):
-
-    return 2 * numpy.abs(sawtooth(f, t)) - 1
-
-
-def chirp(f0, f1, t):
-
-    return numpy.sin(2 * numpy.pi * ((t ** 2) * (f1 - f0) / 2) + t * f0)
-
-def overtones(f, n, t):
+    Parameters
+    ----------
+    f : float
+        Fundamental signal frequency in hertz.
+    n : int
+        Number of additional overtones.
+    t : array
+        Timeline array.
+    """
 
     assert isinstance(n, int)
 
@@ -46,3 +39,84 @@ def overtones(f, n, t):
     o = [a[i - 1] * harmonic(f * i, t) for i in m]
 
     return numpy.stack(o).sum(axis=0) / numpy.sum(a)
+
+
+def square(f, t):
+    """
+    Returns a square shaped wave of the specified frequency.
+
+    Parameters
+    ----------
+    f : float
+        Signal frequency in hertz.
+    t : array
+        Timeline array.
+    """
+
+    return numpy.sign(harmonic(f, t))
+
+
+def sawtooth(f, t):
+    """
+    Returns a sawtooth shaped wave of the specified frequency.
+
+    Parameters
+    ----------
+    f : float
+        Signal frequency in hertz.
+    t : array
+        Timeline array.
+    """
+
+    return 2 * (f * t - numpy.floor(f * t + 0.5))
+
+
+def triangle(f, t):
+    """
+    Returns a triangle shaped wave of the specified frequency.
+
+    Parameters
+    ----------
+    f : float
+        Signal frequency in hertz.
+    t : array
+        Timeline array.
+    """
+
+    return 2 * numpy.abs(sawtooth(f, t)) - 1
+
+
+def chirp(f0, f1, t):
+    """
+    Returns a chirp signal in which the frequency alterates over the time.
+
+    Parameters
+    ----------
+    f0 : float
+        Start signal frequency in hertz.
+    f1 : float
+        End signal frequency in hertz.
+    t : array
+        Timeline array.
+    """
+
+    return numpy.sin(2 * numpy.pi * ((t ** 2) * (f1 - f0) / 2) + t * f0)
+
+
+def noise(s, t):
+    """
+    Returns a random white noise based on the truncated normal distribution.
+
+    Parameters
+    ----------
+    s : float
+        Scale parameter between 0 and 1.
+    t : array
+        Timeline array.
+    """
+
+    a = -1 / s
+    b = +1 / s
+    n = len(t)
+
+    return scipy.stats.truncnorm(a, b, loc=0, scale=s).rvs(n)
